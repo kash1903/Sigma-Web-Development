08646a668c05816d0832e27910dcd6f6577b4f23
Kash Subramanian <kash270800@gmail.com>
2025-02-12 21:06:14 +0530

    new file

---
+++ video 64 (Solution)/compactSolution.js
@@ -0,0 +1,4 @@
+let first = rand < 0.33 ? "crazy" : rand <0.66 && rand>=0.33 ? "amazing" : "fire"
+let second = rand < 0.33 ? "engine" : rand <0.66 && rand>=0.33 ? "foods" : "garments"
+let third = rand < 0.33 ? "bros" : rand <0.66 && rand>=0.33 ? "Limited" : "Hub"
+
